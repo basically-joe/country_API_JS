55e4a33a9936c62cc9652237ce090b444ec36c79
Joe McColl <joe4kool@gmail.com>
2019-01-16 14:20:50 +0000

    drop down populates with country data, tested and working in browser

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const Countries = require('./models/countries.js');
-
+const SelectView = require('./views/select_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 
+  const selectElement = document.querySelector("select#countries");
+  const countryDropdown = new SelectView(selectElement);
+  countryDropdown.bindEvents();
+
   const countries = new Countries();
   countries.getData();
+
+
 });
--- src/views/select_view.js
+++ src/views/select_view.js
@@ -1,7 +1,23 @@
 const PubSub = require('../helpers/pub_sub.js');
 
 const SelectView = function (element) {
-this.element = element;  
+  this.element = element;
+};
+
+SelectView.prototype.bindEvents = function () {
+  PubSub.subscribe("Countries:list-loaded", (event) => {
+    const allCountries = event.detail;
+    this.populate(allCountries);
+  });
+};
+
+SelectView.prototype.populate = function (allCountriesData) {
+  allCountriesData.forEach((country, index) => {
+    const option = document.createElement("option");
+    option.textContent = country.name;
+    option.value = index;
+    this.element.appendChild(option);
+  })
 };
 
 module.exports = SelectView
